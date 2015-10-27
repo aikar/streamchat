7aeb123fda90817c423e8a9976cff928929b488e
Aikar <aikar@aikar.co>
2015-10-27 01:55:19 -0400

    working hitbox and irc connection

--- streamchat.js
+++ streamchat.js
@@ -1,6 +1,9 @@
 "use strict";
 global.co = require('co');
 global.each = require('co-each');
+global.debug = function() {
+	console.log(arguments);
+};
 require('./lib/core').register({
 	'hitbox': require("./lib/hitbox"),
 	'livecoding': require("./lib/livecoding"),
