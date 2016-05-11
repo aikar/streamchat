bda42f691ff367df1752001b64810d66179ce6dd
Aikar <aikar@aikar.co>
2016-05-11 19:38:55 -0400

    integrate streamjar as an iframe

--- public/streamchat.js
+++ public/streamchat.js
@@ -9,6 +9,10 @@
  */
 var last;
 $(function() {
+	var streamjar = getCookie('streamjar');
+	if (streamjar) {
+		$('#streamjar').attr('src', streamjar).show();
+	}
 	var $chat = $('#chat');
 	setInterval(function() {
 		jQuery.ajax('/chat').done( function(data) {
@@ -21,3 +25,9 @@ $(function() {
 		});
 	}, 1500);
 });
+
+function getCookie(name, def) {
+	if (!name) { return def; }
+	var d = decodeURIComponent,x;
+	return (x=document.cookie.match(new RegExp(d(name) + '\\s*=\\s*([^;]*)')))&&d(x[1])||def;
+}
